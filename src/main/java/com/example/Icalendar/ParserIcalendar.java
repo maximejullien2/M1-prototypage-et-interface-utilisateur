0c3b9e43f0343cee8f6c59a103e10b64d77e2e78
Jullien <maxime.jullien2@alumni.univ-avignon.fr>
2024-02-20 11:20:05 +0100

    Update : Récupération de tous les events pour le fichier test.ics et le metter dans un objet ApiCalendar fonctionnel.

--- src/main/java/com/example/Icalendar/ParserIcalendar.java
+++ src/main/java/com/example/Icalendar/ParserIcalendar.java
@@ -20,11 +20,10 @@ public class ParserIcalendar {
 
     public ApiCalendar parseToJson() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(filename)));
-        ArrayList<HashMap<String,String>> outputJson = new ArrayList<HashMap<String,String>>();
         ApiCalendar apiCalendar = new ApiCalendar();
 
         while(!bufferedReader.readLine().equals("BEGIN:VEVENT")){}
-        while(!bufferedReader.readLine().equals("END:VCALENDAR")){
+        while(bufferedReader.readLine()!=null){
             bufferedReader.readLine();
             String text = bufferedReader.readLine();
             String lastModified ;
@@ -39,42 +38,45 @@ public class ParserIcalendar {
             String dateEnd = bufferedReader.readLine().split(":")[1];
             DateEvent dateEvent = new DateEvent(dateStart,dateEnd,lastModified);
             DescriptionEvent descriptionEvent = new DescriptionEvent();
-
             String allContent="";
             String line = bufferedReader.readLine();
-            while (!line.split(";")[0].equals("LOCATION")){
+            while (!line.split(";")[0].equals("LOCATION") && !line.equals("END:VEVENT")){
                 allContent=allContent+line;
                 line = bufferedReader.readLine();
             }
-
-            String summary = allContent.split(";")[1].split(":")[1];
-
-            allContent="";
-            while (!line.split(";")[0].equals("DESCRIPTION")){
-                allContent=allContent+line;
-                line = bufferedReader.readLine();
+            if(line.equals("END:VEVENT")){
+                descriptionEvent.addDescription("TYPE",line.split(":")[1]);
+                Event event = new Event(dateEvent, "","", descriptionEvent);
+                apiCalendar.addEvent(event);
+                bufferedReader.readLine();
             }
+            else {
+                String summary = allContent.split(";")[1].split(":")[1];
+                allContent = "";
+                while (!line.split(";")[0].equals("DESCRIPTION")) {
+                    allContent = allContent + line;
+                    line = bufferedReader.readLine();
+                }
 
-            String location = allContent.split(";")[1].split(":")[1];
-
-            allContent="";
-            while (!line.split(";")[0].equals("X-ALT-DESC")){
-                allContent=allContent+line;
-                line = bufferedReader.readLine();
+                String location = allContent.split(";")[1].split(":")[1];
+                allContent = "";
+                while (!line.split(";")[0].equals("X-ALT-DESC")) {
+                    allContent = allContent + line;
+                    line = bufferedReader.readLine();
+                }
+                descriptionEvent.addDescription(allContent.split(";")[1].split(Pattern.quote("\\n"))[0].split(":")[1], allContent.split(";")[1].split(Pattern.quote("\\n"))[0].split(":")[2]);
+                for (int i = 1; i < allContent.split(";")[1].split(Pattern.quote("\\n")).length; i++) {
+                    String key = allContent.split(";")[1].split(Pattern.quote("\\n"))[i].split(":")[0];
+                    String value = allContent.split(";")[1].split(Pattern.quote("\\n"))[i].split(":")[1];
+                    System.out.println("Key = "+key);
+                    System.out.println("Value = "+value);
+                    descriptionEvent.addDescription(key, value);
+                }
+                Event event = new Event(dateEvent, summary, location, descriptionEvent);
+                apiCalendar.addEvent(event);
+                while (!bufferedReader.readLine().equals("BEGIN:VEVENT")) {
+                }
             }
-            descriptionEvent.addDescription(allContent.split(";")[1].split(Pattern.quote("\\n"))[0].split(":")[1],allContent.split(";")[1].split(Pattern.quote("\\n"))[0].split(":")[2]);
-            //System.out.println("###########");
-            for (int i = 1 ; i<allContent.split(";")[1].split(Pattern.quote("\\n")).length;i++){
-                String key = allContent.split(";")[1].split(Pattern.quote("\\n"))[i].split(":")[0];
-                String value = allContent.split(";")[1].split(Pattern.quote("\\n"))[i].split(":")[1];
-                /*System.out.println("key = "+key);
-                System.out.println("value = "+value);
-                System.out.println("###########");*/
-                descriptionEvent.addDescription(key,value);
-            }
-            Event event = new Event(dateEvent,summary,location,descriptionEvent);
-            apiCalendar.addEvent(event);
-            while(!bufferedReader.readLine().equals("BEGIN:VEVENT")){}
         }
         return apiCalendar;
     }
